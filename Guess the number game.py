d47e957ab8c5929363c5b02a8f0cac995b5d763e
Mohammad SN <shahnazi2002@gmail.com>
2021-11-08 08:33:03 +0330

    Update Guess the number game.py

--- Guess the number game.py
+++ Guess the number game.py
@@ -1,21 +1,21 @@
 from random import randint
 
 print("Start!")
-goal = randint(1,100)
+goal = randint(1, 100)
 repeat = True
 
-while repeat :
+while repeat:
     answer = int(input("Enter a number between 1 and 100: "))
-    if answer == goal :
+    if answer == goal:
         print("Yes!")
         print("You reached the goal!")
         print("Good job!")
         repeat = False
-    elif answer < goal :
+    elif answer < goal:
         print("No!")
         print("The goal is bigger!")
         print("Try again!")
-    elif answer > goal :
+    elif answer > goal:
         print("No!")
         print("The goal is smaller!")
         print("Try again!")
